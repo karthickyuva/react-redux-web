31a7b9fae2f6102cbe0d37a4c24f037023fea3c3
karthickthidiff <karthick@thidiff.com>
2018-07-28 23:17:31 +0530

    Updated list view pagination and removed unwanted logs

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,38 +1,43 @@
-import React from 'react';
+import React, { Component } from "react";
 import ListPagination from './ListPagination';
 import ArticlePreview from "../components/ArticlePreview";
 
-const ArticleList = props => {
-  if (!props.articles) {
-    return (
-      <div className="article-preview">Loading...</div>
-    );
-  }
+class ArticleList extends Component{
 
-  if (props.articles.length === 0) {
-    return (
-      <div className="article-preview">
-        No articles are here... yet.
-      </div>
-    );
-  }
+	render() {
+		const { articles, articlesCount, currentPage } = this.props;
 
-  return (
-    <div>
-      {
-        props.articles.map(article => {
-          return (
-            <ArticlePreview article={article} key={article.slug} />
-          );
-        })
-      }
+		if (!articles) {
+			return (
+                <div className="article-preview">Loading...</div>
+			);
+		}
 
-      <ListPagination
-        pager={1}
-        articlesCount={props.articlesCount}
-        currentPage={props.currentPage} />
-    </div>
-  );
-};
+		if (articles.length === 0) {
+			return (
+                <div className="article-preview">
+                  No articles are here... yet.
+                </div>
+			);
+		}
+
+		return (
+            <div>
+				{
+					articles.map(article => {
+						return (
+                            <ArticlePreview article={article} key={article.slug}/>
+						);
+					})
+				}
+
+              <ListPagination
+                  pager={1}
+                  articlesCount={articlesCount}
+                  currentPage={currentPage}/>
+            </div>
+		);
+	};
+}
 
 export default ArticleList;
